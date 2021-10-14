0aa47aa78f6be41c61134d43cfaa9aa3e06f0879
Arnab-31 <57807670+Arnab-31@users.noreply.github.com>
2021-10-14 22:48:27 +0530

    Mix-max algo optimized using aplha-beta pruning

--- src/ChessAI.js
+++ src/ChessAI.js
@@ -10,7 +10,7 @@ const minimaxRoot =function(depth, game, isMaximisingPlayer) {
     for(var i = 0; i < newGameMoves.length; i++) {
         var newGameMove = newGameMoves[i];
         game.move(newGameMove);
-        var value = minimax(depth - 1, game, !isMaximisingPlayer);
+        var value = minimax(depth - 1, game, -10000, 10000, !isMaximisingPlayer);
         game.undo();
         if(value >= bestMove) {
             bestMove = value;
@@ -20,7 +20,7 @@ const minimaxRoot =function(depth, game, isMaximisingPlayer) {
     return bestMoveFound;
 };
 
-const minimax = function (depth, game, isMaximisingPlayer) {
+const minimax = function (depth, game, alpha, beta, isMaximisingPlayer) {
     //positionCount++;
     if (depth === 0) {
         return -evaluateBoard(game.board());
@@ -32,16 +32,24 @@ const minimax = function (depth, game, isMaximisingPlayer) {
         var bestMove = -9999;
         for (var i = 0; i < newGameMoves.length; i++) {
             game.move(newGameMoves[i]);
-            bestMove = Math.max(bestMove, minimax(depth - 1, game, !isMaximisingPlayer));
+            bestMove = Math.max(bestMove, minimax(depth - 1, game, alpha, beta, !isMaximisingPlayer));
             game.undo();
-        }
+            alpha = Math.max(alpha, bestMove);
+            if (beta <= alpha) {
+                return bestMove;
+            }
+        } 
         return bestMove;
     } else {
         var bestMove = 9999;
         for (var i = 0; i < newGameMoves.length; i++) {
             game.move(newGameMoves[i]);
-            bestMove = Math.min(bestMove, minimax(depth - 1, game, !isMaximisingPlayer));
+            bestMove = Math.min(bestMove, minimax(depth - 1, game, alpha, beta, !isMaximisingPlayer));
             game.undo();
+            beta = Math.min(beta, bestMove);
+            if (beta <= alpha) {
+                return bestMove;
+            }
         }
         return bestMove;
     }
